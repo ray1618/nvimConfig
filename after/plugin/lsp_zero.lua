9af3090bb8fd0be29c9d3745ac85ab4371b6b8d2
Raymond Bas <raymond.bas@cito.nl>
2024-03-04 11:19:26 +0100

    added enter as select lsp option

--- after/plugin/lsp_zero.lua
+++ after/plugin/lsp_zero.lua
@@ -50,6 +50,7 @@ cmp.setup({
 		['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
 		['<C-y>'] = cmp.mapping.confirm({ select = true }),
 		['<Tab>'] = cmp.mapping.confirm({ select = true }),
+		['<CR>'] = cmp.mapping.confirm({ select = true }),
 		['<C-Space>'] = cmp.mapping.complete(),
 	}),
 })
