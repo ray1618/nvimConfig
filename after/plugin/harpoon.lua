0c064cd2ae1294422731993b8cf9b82435819ecd
Raymond Bas <raymond.bas@cito.nl>
2024-03-15 10:44:47 +0100

    changed to correct version angular lsp and change harpoon shortcut

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -12,6 +12,6 @@ vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
 vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
 vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
 
--- Toggle previous & next buffers stored within Harpoon list
-vim.keymap.set("n", "<C-P>", function() harpoon:list():prev() end)
+-- Toggle (b)revious? & next buffers stored within Harpoon list
+vim.keymap.set("n", "<C-B>", function() harpoon:list():prev() end)
 vim.keymap.set("n", "<C-N>", function() harpoon:list():next() end)
