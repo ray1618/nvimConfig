229af74ff171df918d4fe8895fcfa322200b0ae4
Raymond Bas <raymond.bas@cito.nl>
2025-02-03 08:53:15 +0100

    added ignore whitespaces to gitblame

--- after/plugin/gitsigns.lua
+++ after/plugin/gitsigns.lua
@@ -21,7 +21,7 @@ require('gitsigns').setup {
 		virt_text = true,
 		virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
 		delay = 1000,
-		ignore_whitespace = false,
+		ignore_whitespace = true,
 		virt_text_priority = 100,
 	},
 	current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
